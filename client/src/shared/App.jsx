2c0d8c2a392bcd2d81c3f0b1404e4ace3fbb5b9b
neisha1618 <neisha1618@gmail.com>
2020-06-11 15:29:05 -0500

    route to the different components

--- client/src/shared/App.jsx
+++ client/src/shared/App.jsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import { render } from 'react-dom';
 import Navbar from './Navbar.jsx';
 import Explore from '../explore/Explore.jsx';
+import MovementList from './MovementList.jsx';
 
 const App = () => {
   return (
