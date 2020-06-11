6d02ff2932806caf648f07ad94e212d4a3376a06
Will Preble <wpreble1@gmail.com>
2020-06-11 16:22:52 -0500

    Add fakeData for testing front-end components.

---
+++ client/src/shared/fakeData.js
@@ -0,0 +1,38 @@
+const fakeCampaigns = [
+  {
+    id: 1,
+    imageUrl: 'https://assets.change.org/photos/0/la/lt/mylaLTYqhScqWHI-800x450-noPad.jpg?1590516227',
+    name: 'Justice for George Floyd',
+    location: 'Minneapolis, MN',
+    description: `
+      George Floyd was murdered by a Minneapolis police officer.
+
+      George was handcuffed and restrained and being completely cooperative when this all went down. The officer put his knee on George’s neck choking him for minutes on minutes while George screamed that he could not breathe. Bystanders beg for the police officer to take his knee off George’s neck, but the officer didn’t listen and continued to choke him.
+
+      Not that it would matter at all, but George was not even wanted for a violent crime. A grocery store that he was signing a bad check. 
+
+      We are trying to reach the attention of Mayor Jacob Frey and DA Mike Freeman to beg to have the officers involved in this disgusting situation fired and for charges to be filed immediately.
+
+      Please help us get justice for George and his family!
+    `,
+    followers: 101312,
+    emailsSent: 9000,
+  },
+  {
+    id: 2,
+    imageUrl: 'https://assets.change.org/photos/5/tg/gk/uNtGGkZoxgRnuuz-800x450-noPad.jpg?1589086374',
+    name: 'Justice for Breonna Taylor',
+    location: 'Louisville, KY',
+    description: `
+      Breonna Taylor was an award-winning EMT and model citizen. She loved her family and community. She worked at two hospitals as an essential worker during the pandemic.
+
+      One month ago, a division of the Louisville Police Department performed an illegal, unannounced drug raid on her home. Not a single officer announced themselves before ramming down her door and firing 22 shots, shooting Breonna 8 times, killing her. 
+
+      Not only were the police at the WRONG HOUSE, but the man they were looking for had already been arrested earlier that day.
+    `,
+    followers: 10999,
+    emailsSent: 65,
+  },
+];
+
+export default fakeCampaigns;
