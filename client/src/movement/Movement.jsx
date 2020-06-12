ab353a28e32b4849f9d7892dd40fa45e8af1f132
neisha1618 <neisha1618@gmail.com>
2020-06-11 19:10:27 -0500

    add skelaton to movement and profile

---
+++ client/src/movement/Movement.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const Movement = () => {
+  return (
+    <div>
+      <strong>Movement Page</strong>
+    </div>
+  );
+};
+export default Movement;
---
+++ client/src/profile/Profile.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const Profile = () => {
+  return (
+    <div>
+      <strong>Profile Page</strong>
+    </div>
+  );
+};
+export default Profile;
