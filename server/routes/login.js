4b82c3b795d9be3bc2af5fb1493a8ef941e6a237
Clayton Christenson <cchristenson.opspark@gmail.com>
2020-06-15 10:12:38 -0500

    uncomment post

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,21 +1,25 @@
+import ReactDOMServer from 'react-dom/server';
+
 const passport = require('passport');
 const express = require('express');
 const { User } = require('../db/index');
 
 
+
 const router = express.Router();
 
 router.get('/login', (req, res) => {
- console.log(req);
-  res.send('login');
+  console.log(req);
+  // go to db and find user data 
+  // send data back the client side 
 });
 
-// router.post('/login', passport.authenticate('local'),
-//   (req, res) => {
-//     console.log('logged in', req.user);
-//     const userInfo = {
-//       username: req.user.username,
-//     };
-//     res.send(userInfo);
-//   });
+router.post('/login',
+  (req, res) => {
+    console.log('logged in', req.user);
+    const userInfo = {
+      username: req.user.username,
+    };
+    res.send(userInfo);
+  });
 module.exports.router = router;
