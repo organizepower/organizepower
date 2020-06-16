07f52e4977a852e3974eeb6d8fbf5806f496c74f
Clayton Christenson <cchristenson.opspark@gmail.com>
2020-06-16 14:15:27 -0500

    create auth directory in server, add passport.js and passwordUtils.js

---
+++ server/auth/passport.js
@@ -0,0 +1,29 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const { User } = require('../db/index');
+
+passport.use(new LocalStrategy((username, password, cb) => {
+  User.findOne({ username: username })
+    .then((user) => {
+      if (!user) { return cb(null, false) }
+
+      // Function defined at bottom of app.js
+      const isValid = validPassword(password, user.hash, user.salt);
+
+      if (isValid) {
+        return cb(null, user);
+      }
+      return cb(null, false);
+    })
+    .catch((err) => {
+      cb(err);
+    });
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
