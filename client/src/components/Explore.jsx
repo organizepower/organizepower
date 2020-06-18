7dd2cd91f0fccdc9c776614b84f5a5fcdc0d5502
masonkristopher <masonk000@gmail.com>
2020-06-18 10:57:13 -0500

    move & build out getMovements function for explore page

--- client/src/components/Explore.jsx
+++ client/src/components/Explore.jsx
@@ -1,19 +1,21 @@
 /* eslint-disable import/extensions */
 import React, { useState, useEffect } from 'react';
-
+import axios from 'axios'
 import { getMovements } from '../services/services';
 import { fakeMovements } from '../services/fakeData';
 import MovementList from './MovementList.jsx';
 
 const Explore = ({ user, handleClick }) => {
-  const [movements, setMovements] = useState(fakeMovements);
+  const [movements, setMovements] = useState([]);
 
-  // example of a function that could be used to update movements
-  // this is currently returning a 404 error
+  // get all movements in the database
   useEffect(() => {
     getMovements()
-      .then(results => setMovements(results));
-  });
+      .then(results => {
+        setMovements(results.data);
+      })
+      .catch(err => console.error(err));
+  }, []);
 
   return (
     <div>
