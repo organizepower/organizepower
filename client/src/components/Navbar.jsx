9e837497b7d64cf3a8f4951ca5befc0ec6fb8865
masonkristopher <masonk000@gmail.com>
2020-06-18 16:25:28 -0500

    fix bug in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -18,8 +18,6 @@ import { getUserProfileById } from '../services/services';
 
 import { logout } from '../services/services';
 
-const Navbar = ({ user, setUserState }) => {
-
 const Navbar = () => {
   // const { id } = currentMovement;
   const [currentMovement, setCurrentMovement] = useState({});
@@ -102,78 +100,5 @@ const Navbar = () => {
     </Router>
   );
 };
-
-// these commented out functions makes it so that the nav bar items are on every page
-
-// function MovementList() {
-//   return (
-//     <div>
-//       <h2>MovementList</h2>
-//     </div>
-//   );
-// }
-
-// function Profile() {
-//   return (
-//     <div>
-//       <h2>Profile</h2>
-//     </div>
-//   );
-// }
-
-// function Explore() {
-//   return (
-//     <div>
-//       <h2>Explore</h2>
-//     </div>
-//   );
-// }
-
-// function Login() {
-//   return (
-//     <div>
-//       <h2>Login</h2>
-//     </div>
-//   );
-// }
-
-{ /* <Router>
-<strong>OrganizePower</strong>
-<div>
-  <nav>
-    <ul>
-      <li>
-        <Link to="/MovementList">MovementList </Link>
-      </li>
-      <li>
-        <Link to="/Profile">Profile </Link>
-      </li>
-      <li>
-        <Link to="/Explore">Explore </Link>
-      </li>
-      <li>
-        <Link to="/Login">Login/signUp </Link>
-      </li>
-    </ul>
-  </nav>
-
-  {/* A <Switch> looks through its children <Route>s and
-      renders the first one that matches the current URL. */ }
-//   <Switch>
-//     <Route path="/MovementList">
-//       <MovementList />
-//     </Route>
-//     <Route path="/profile">
-//       <Profile />
-//     </Route>
-//     <Route path="/Explore">
-//       <Explore />
-//     </Route>
-//     <Route path="/login/signup">
-//       <Login />
-//     </Route>
-//   </Switch>
-// </div>
-// </Router> */}
 
 export default Navbar;
