b713d9b93c51a52edd9c20703c465ad9e91ff93a
masonkristopher <masonk000@gmail.com>
2020-06-19 11:24:13 -0500

    add redirects to Login component

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -6,42 +6,45 @@ import auth from '../services/auth';
 const Login = ({ setUser }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [authStatus, setAuthStatus] = useState('');
+  const [userId, setUserId] = useState('');
+
   const handleClick = () => {
+    // debugger;
     axios.post('/login', { username, password })
       .then(({ data }) => {
         const { message } = data;
-        debugger;
-        if (message === 'invalidUser') {
-          // redirect to signup
-
-        } else if (message === 'invalidPassword') {
-          // redirect to login
-
-        } else if (message === 'success') {
+        if (message === 'success') {
           const { user } = data;
+          setUserId(user.id);
           setUser(user);
-          // redirect to profile page
-        } else {
-          // redirect to login?
         }
+        setAuthStatus(message);
       })
       .catch(err => console.error(err));
   };
 
   return (
-    <form>
-      <div>
-        <p>Username:</p>
-        <input type="text" name="username" onChange={(e) => setUsername(e.target.value)} />
-      </div>
-      <div>
-        <p>Password:</p>
-        <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} />
-      </div>
-      <div>
-        <input type="submit" value="Log In" onClick={handleClick} />
-      </div>
-    </form>
+    <div>
+      <form>
+        <div>
+          <p>Username:</p>
+          <input type="text" name="username" onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div>
+          <p>Password:</p>
+          <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <div>
+          <input type="submit" value="Log In" onClick={handleClick} />
+        </div>
+      </form>
+
+      {authStatus === 'invalidUser' && <Redirect to="/signup" />}
+      {authStatus === 'invalidPassword' && <Redirect to="/login" />}
+      {authStatus === 'success' && <Redirect to={`/profile/${userId}`} />}
+
+    </div>
   );
 };
 
