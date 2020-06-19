0b3b627450633769e23134ceb302d81e2aa3d93c
masonkristopher <masonk000@gmail.com>
2020-06-19 11:24:34 -0500

    add maxAge to cookie options in app.js

--- server/app.js
+++ server/app.js
@@ -39,8 +39,8 @@ app.use(session({
   cookie: {
     sameSite: 'None',
     secure: true, // requires HTTPS connection
-    // maxAge: 1000 * 60 * 60 * 24
-    // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
+    maxAge: 1000 * 60 * 60 * 24, // one day
+    // maxAge: 1000 * 60, // one minute
   },
 }));
 
