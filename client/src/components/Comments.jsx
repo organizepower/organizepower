6a703bfe73462b43a1c1ab226437d72dd01d8d47
neisha1618 <neisha1618@gmail.com>
2020-06-20 16:50:34 -0500

    comment component and routes semi set

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,79 +1,67 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const CommentList = ({ data }) => {
-  // console.log(data)
-  // const commentNodes = data.map((comment) => {
-  //   console.log(data);
-  //   return (
-  //     <div>
-  //     <Comment author={comment.author} key={comment.id}>
-  //       {comment.text}
-  //     </Comment>
-  //     </div>
-  //   );
-  // });
+// sub component
+const CommentList = ({ author, text }) => {
   return (
     <div>
-      <Comment data={data} />
+      <CommentListItem author={author} text={text} />
     </div>
   );
 };
-
-const Comment = ({ data }) => {
-  console.log(data);
+// sub component
+const CommentListItem = ({ author, text }) => {
   return (
     <div className="comment">
       <h2 className="commentAuthor">
-        {data[0].author}
-
+        {author}
       </h2>
-      {data[0].text}
+      <h2 className="commentText">
+        {text}
+      </h2>
     </div>
   );
 };
 
-// const CommentList = ({ data }) => {
-//   // comment box component
-//   return (
-//     <div className="commentBox">
-//       <h1>WOO My Comment Box</h1>
-//       <CommentList data={data} />
-//       <CommentForm />
-//     </div>
-//   );
-// };
-
-const Comments = ({data}) => {
-  const [author, setAuthor] = useState('');
+// lead component
+const Comments = ({ movement, user }) => {
+  const { id } = movement;
+  // const [author, setAuthor] = useState('');
   const [text, setText] = useState('');
+  const [comments, setComments] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('clicked');
+    axios.post('movement/comments', { movement: id, comment: text, author: user.id })
+      .then((response) => console.log(response))
+      .then(() => {
+        axios.get('movement/comments')
+          .then((response) => {
+            console.log(response.data);
+            setComments(response.data);
+          })
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err));
+    // setText(e.target.value);
   };
 
   return (
     <div>
-    <form className="commentForm" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Your name"
-        value={author}
-        onChange={e => setAuthor(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Say something..."
-        value={text}
-        onChange={e => setText(e.target.value)}
-      />
-      <input type="submit" value="Post" />
-    </form>
-     <div className="commentBox">
-      <h1>WOO My Comment Box</h1>
-      <CommentList data={data} />
-    </div>
+      {user && (
+        <form className="commentForm" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Say something..."
+            onChange={e => setText(e.target.value)}
+          />
+          <input type="submit" value="Post" />
+        </form>
+      )}
+      <div className="commentBox">
+        <strong>Comments</strong>
+        <CommentList comments={comments} />
+      </div>
     </div>
   );
 };
