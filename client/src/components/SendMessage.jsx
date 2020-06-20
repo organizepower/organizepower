634fd9165483f7aa1de1d1f98734500606d50285
masonkristopher <masonk000@gmail.com>
2020-06-20 14:10:00 -0500

    minor code cleanup

--- client/src/components/SendMessage.jsx
+++ client/src/components/SendMessage.jsx
@@ -31,7 +31,6 @@ ${urlLink}`;
     event.preventDefault();
     document.getElementById('send-text').reset();
     setmessageButton('Sending Text...');
-    console.log(event)
     axios.post('/twilio', { to, body })
       .then(() => {
         setStatus('sent');
