892b759c0e6cb2655434d49d254beec7034461e0
Will Preble <wpreble1@gmail.com>
2020-06-21 11:29:56 -0500

    Improve signup form. Add pseudocode to login and signup

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,29 +3,34 @@ import { Redirect } from 'react-router-dom';
 
 import { login } from '../services/services';
 
+/**
+ * @param {function} setUser - sets the user state on the navbar component
+ * @param {function} setIsAuthenticated - sets the setIsAuthenticated state on th navbar component
+ * @param {function} setIsNewUser - sets the isNewUser state on the navbar component
+ * (only used to change the state back to false)
+ * @param {boolean} isNewUser - state on navbar that will be true if a user just signed up
+ */
 const Login = ({
   setUser,
   setIsAuthenticated,
   setIsNewUser,
   isNewUser,
 }) => {
+  // states managed for form submission
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  // state managed to handle redirects after form submission
   const [authStatus, setAuthStatus] = useState('');
   const [userId, setUserId] = useState('');
-  // sending post request to login with username and password
-  // then receive a response with a status message and user object if login is successful
-  // set the userId and authStatus states on Login.jsx
-  // set the user state on Navbar.jsx
-  // the authStatus will determine which page the user is redirected to
 
-  const handleClick = (event) => {
+  const handleLoginSubmit = event => {
     event.preventDefault();
     login(username, password)
-      .then(({ data }) => {
-        const { message, user } = data;
+      .then(({ data: { message, user } }) => {
         if (message === 'invalidPassword') {
-          document.getElementById('login').reset();
+          setUsername('');
+          setPassword('');
         }
         if (message === 'success') {
           setUserId(user.id);
@@ -40,25 +45,29 @@ const Login = ({
 
   return (
     <div className="w-full max-w-xs m-4">
+      {/* Message conditionally rendered if the user just signed up */}
       {isNewUser && <p>Thank you for signing up! Please log in.</p>}
       <form id="login" className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2">
             Username:
           </label>
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="username" onChange={(e) => setUsername(e.target.value)} />
+          <input value={username} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="username" onChange={(e) => setUsername(e.target.value)} />
         </div>
         <div className="mb-6">
           <label className="block text-gray-700 text-sm font-bold mb-2">
             Password:
           </label>
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="password" name="password" onChange={(e) => setPassword(e.target.value)} />
+          <input value={password} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="password" name="password" onChange={(e) => setPassword(e.target.value)} />
           {authStatus === 'invalidPassword' && <p className="italic text-xs text-red-500">Invalid password - try again!</p>}
         </div>
         <div className="flex items-center justify-between">
-          <input className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" value="Log In" onClick={handleClick} />
+          <input className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" value="Log in" onClick={handleLoginSubmit} />
         </div>
       </form>
+      {/* invalid user will redirect to signup */}
+      {/* invalid password will clear form and reload login */}
+      {/* success will redirect to the profile of the user */}
       {authStatus === 'invalidUser' && <Redirect to="/signup" />}
       {authStatus === 'invalidPassword' && <Redirect to="/login" />}
       {authStatus === 'success' && <Redirect to={`/profile/${userId}`} />}
